5c750669b1173cbd572dca4c612c87186dc46f03
mrcsporto <mrcsporto@gmail.com>
2022-11-09 16:21:36 -0300

    update style

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,20 +3,20 @@ import './Banner.css'
 export default function Banner(options) {
     return (
 
-        <section className='maim-banner'>
-            <div className='maim-banner-container maim-banner-text'>
-                <div className='maim-banner-text'>                    
+        <section className='main-banner'>
+            <div className='main-banner-container'>
+                <div className='main-banner-text'>                    
                     <h2>Melhores ofertas personalizadas</h2>
-                    <h1>Queima de stoque Nike 🔥</h1>
+                    <h1>Queima de Estoque Nike 🔥</h1>
                     <p>
                         Consequat culpa exercitation mollit nisi excepteur do
                         do tempor laboris eiusmod irure consectetur.
                     </p>
-                    <button className='maim-banner-button'>
+                    <button className='main-banner-button'>
                         ver ofertas
                     </button>
                 </div>
-                <div className='maim-banner-imagem'>
+                <div className='main-banner-imagem'>
                     <img src="/images/tenis_banner.png" alt="tenis_banner" />   
                 </div>
 
