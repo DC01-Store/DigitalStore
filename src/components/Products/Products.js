1acc593c677ee6107a87c1ebda678e2efdef93ef
mrcsporto <mrcsporto@gmail.com>
2022-11-09 23:27:25 -0300

    add discout price feature

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -23,34 +23,36 @@ const UsingFetch = () => {
 
   return (
     <> 
-    <section className='best-seller-sneakers'>
-      <div className="container">
-        <div className="content">
-            {sneakers.map(({discount, price, description, category}) => (
-                  <div className="card">
-                    <div className="content-wrap">
-                      { discount !== "" 
-                                ? <div className="discount">{discount}{"% OFF"}</div>
-                                : <div className="discount-hidden" style="visibility: hidden"></div>
-                            }
-                        <div className='sneaker-image'>
-                          <img src="/images/sneaker.png" alt="tenis_banner" />   
-                        </div>
+      <section className='best-seller-sneakers'>
+        <div className="container">
+          <div className="content">
+              {sneakers.map(({discount, price, description, category}) => (
+                    <div className="card">
+                      <div className="content-wrap">
+                        { discount !== 0
+                                  ? (<div className="discount">{discount}{"% OFF"}</div>)
+                                  : (<div className="discount" style={{visibility: "hidden"}}></div>)
+                              } 
+                          <div className='sneaker-image'>
+                            <img src="/images/sneaker.png" alt="tenis_banner" />   
+                          </div>
+                      </div>
+                        <div className="category">{category}</div>
+                        <div className="description">{description}</div>
+                        <div> 
+                          { discount
+                            ? (<div className="firstprice">{price}<span className="price"> <span style={{visibility: "hidden"}}>.</span>  {(price-((discount/100)*price)).toFixed(2)}</span></div>)
+                            : (<div className="price">{price}</div>)
+                          }
+                        </div>   
                     </div>
-                      <div className="category">{category}</div>
-                      <div className="description">{description}</div>
-                      <div className="price">{price}</div>
-                          
-                  </div>
-                ))
-            }
+                  ))
+              }
+          </div>
         </div>
-      </div>
-    </section>
-
+      </section>
     </>
   )
-
 }
 
 export default UsingFetch
